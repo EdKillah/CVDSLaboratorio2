a7d6617302f81fce34b7124fcff9aa7ebbfef6b0
csarssj <47017021+csarssj@users.noreply.github.com>
2020-01-28 20:31:21 -0500

    parte saludo

--- Patterns/src/main/java/edu/eci/cvds/patterns/App.java
+++ Patterns/src/main/java/edu/eci/cvds/patterns/App.java
@@ -8,6 +8,12 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        if (args!=null){
+			for (int i=0; i<args.length; i++){
+        	System.out.print( args[i] + " ");
+			}
+    	}else{
+        	System.out.println( "Hello World!" );
+    	}
     }
 }
